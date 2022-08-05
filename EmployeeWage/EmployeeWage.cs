9540102a1fc8eb1adfe71127114bf0782644654f
Kundan <kundan kamble>
2022-08-05 02:48:24 -0800

    Welcome to Employee Wage Computation

--- EmployeeWage/EmployeeWage.cs
+++ EmployeeWage/EmployeeWage.cs
@@ -8,39 +8,11 @@ namespace EmployeeWage
 {
     public class EmployeeWage
     {
-        public const int empRatePrHr = 20;
-        public const int isFullTime = 1;
-        public const int isPartTime = 2;
-        public const int numOfDays = 20;
-        public const int maxHrs = 100;
-        public void EmpWageForTillCondition()
+        public void EmpLoyee() 
         {
-            int workHr;
-            int totalWrkHr = 0;
-            int totalWage;
-            int totalWrkDays = 0;
-
-            while (totalWrkHr < maxHrs && totalWrkDays < numOfDays)
-            {
-                int employeeStatus = new Random().Next(2);
-                switch (employeeStatus)
-                {
-                    case isFullTime:
-                        workHr = 8;
-                        break;
-                    case isPartTime:
-                        workHr = 4;
-                        break;
-                    default:
-                        workHr = 0;
-                        break;
-                }
-                totalWrkHr += workHr;
-                totalWrkDays++;
-            }
-            totalWage = totalWrkHr * empRatePrHr;
-            Console.WriteLine("Employee total wage is " + totalWage
-                + " for {0} working Days", totalWrkDays);
+            Console.WriteLine("Welcome to Employee Wage Computation");
+           
         }
+
     }
 }
