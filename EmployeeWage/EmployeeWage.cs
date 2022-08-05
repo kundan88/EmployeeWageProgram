cda3b2d2cdb2804f2975d18f0a62b843c266ea91
Kundan <kundan kamble>
2022-08-05 02:35:13 -0800

    Added employee wage for month case Program

--- EmployeeWage/EmployeeWage.cs
+++ EmployeeWage/EmployeeWage.cs
@@ -9,32 +9,33 @@ namespace EmployeeWage
     public class EmployeeWage
     {
         public const int empRatePrHr = 20;
-        public const int isFullTime = 2;
-        public const int isPartTime = 1;
-
-        public void EmpWageUsingSwitchCase()
+        public const int isFullTime = 1;
+        public const int isPartTime = 2;
+        public const int numOfDays = 20;
+        public void EmpWageForMonth()
         {
-
             int workHr;
-            int totalWage;
-
-            int employeeStatus = new Random().Next(2);
-
-            switch (employeeStatus)
+            int empWage;
+            int totalWage = 0;
+            for (int days = 0; days < numOfDays; days++)
             {
-                case isFullTime:
-                    workHr = 8;
-                    break;
-                case isPartTime:
-                    workHr = 4;
-                    break;
-                default:
-                    workHr = 0;
-                    break;
+                int employeeStatus = new Random().Next(2);
+                switch (employeeStatus)
+                {
+                    case isFullTime:
+                        workHr = 8;
+                        break;
+                    case isPartTime:
+                        workHr = 4;
+                        break;
+                    default:
+                        workHr = 0;
+                        break;
+                }
+                empWage = empRatePrHr * workHr;
+                totalWage += empWage;
             }
-            totalWage = empRatePrHr * workHr;
             Console.WriteLine("Employee total wage is " + totalWage);
         }
     }
 }
-
